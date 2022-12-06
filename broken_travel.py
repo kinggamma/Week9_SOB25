ad77a3c13331dc84fc55d025d77f38407f0fa3f6
Ninas <hjdfkhdfk>
2022-12-06 13:50:10 +0400

    travel

--- broken_travel.py
+++ broken_travel.py
@@ -8,7 +8,7 @@
 
 year = int.input("Greetings! What is your year of origin? ")
 
-if year <= 1900
+if year <= 1900:
     print ('Woah, that\'s the past!')
 elif year > 1900 & year < 2020:
     print ("That's totally the present!")
